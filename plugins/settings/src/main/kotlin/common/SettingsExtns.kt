5fdd1506a0b4c93d0c836c599aa710fe8c69b07f
Suresh G <sureshg.g@gmail.com>
2025-02-20 11:15:19 -0800

    chore: misc updates

--- plugins/settings/src/main/kotlin/common/SettingsExtns.kt
+++ plugins/settings/src/main/kotlin/common/SettingsExtns.kt
@@ -3,8 +3,17 @@ package common
 import org.gradle.api.initialization.Settings
 import org.gradle.api.provider.Provider
 
+fun Settings.gradleBooleanProperty(name: String): Provider<Boolean> =
+    providers.gradleProperty(name).map(String::toBoolean).orElse(false)
+
 val Settings.isNativeTargetEnabled: Boolean
   get() = gradleBooleanProperty("kotlin.target.native.enabled").get()
 
-fun Settings.gradleBooleanProperty(name: String): Provider<Boolean> =
-    providers.gradleProperty(name).map(String::toBoolean).orElse(false)
+val Settings.isWinTargetEnabled: Boolean
+  get() = gradleBooleanProperty("kotlin.target.win.enabled").get()
+
+val Settings.isComposeEnabled: Boolean
+  get() = gradleBooleanProperty("compose.enabled").get()
+
+val Settings.isSpringBootEnabled: Boolean
+  get() = gradleBooleanProperty("springboot.enabled").get()
