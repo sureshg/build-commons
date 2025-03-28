e818de96fc7968dfc5e7e91f29be743b7e0a93e8
Suresh G <sureshg.g@gmail.com>
2025-03-28 01:09:15 -0700

    chore: build-logic refactorings

--- plugins/settings/src/main/kotlin/common/SettingsExtns.kt
+++ plugins/settings/src/main/kotlin/common/SettingsExtns.kt
@@ -3,17 +3,17 @@ package common
 import org.gradle.api.initialization.Settings
 import org.gradle.api.provider.Provider
 
-fun Settings.gradleBooleanProperty(name: String): Provider<Boolean> =
+fun Settings.gradleBooleanProp(name: String): Provider<Boolean> =
     providers.gradleProperty(name).map(String::toBoolean).orElse(false)
 
 val Settings.isNativeTargetEnabled: Boolean
-  get() = gradleBooleanProperty("kotlin.target.native.enabled").get()
+  get() = gradleBooleanProp("kotlin.target.native.enabled").get()
 
 val Settings.isWinTargetEnabled: Boolean
-  get() = gradleBooleanProperty("kotlin.target.win.enabled").get()
+  get() = gradleBooleanProp("kotlin.target.win.enabled").get()
 
 val Settings.isComposeEnabled: Boolean
-  get() = gradleBooleanProperty("compose.enabled").get()
+  get() = gradleBooleanProp("compose.enabled").get()
 
 val Settings.isSpringBootEnabled: Boolean
-  get() = gradleBooleanProperty("springboot.enabled").get()
+  get() = gradleBooleanProp("springboot.enabled").get()
