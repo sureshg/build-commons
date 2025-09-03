fb815faed7820afaf5d4871e8c832b101964cf29
Suresh G <sureshg.g@gmail.com>
2025-09-03 09:43:07 -0700

    chore: remove kopy from sandbox

--- sandbox/src/jvmMain/kotlin/App.kt
+++ sandbox/src/jvmMain/kotlin/App.kt
@@ -1,12 +1,10 @@
 package dev.suresh
 
-import com.javiersc.kotlin.kopy.Kopy
-
-@Kopy data class Lang(val name: String)
+data class Lang(val name: String)
 
 fun main() {
   println("Hello, ${BuildConfig.name}! 🏖")
   println("Java: ${BuildConfig.java}, Kotlin: ${BuildConfig.kotlin}")
   println("Runtime Version: ${JApp.runtimeVersion()}")
-  println(Lang("Java").copy { name = "Kotlin" })
+  println(Lang("Java").copy(name = "Kotlin"))
 }
